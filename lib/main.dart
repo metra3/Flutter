6228f97b167cf714ab623e1289b0739908da0d27
metra3 <camelyemeks@gmail.com>
2022-09-07 17:55:48 +0300

    Temel Değişkenler

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,14 @@
 void main()
 {
-  print('Hello World');
+  String mesaj = "Selam mete";
+  int DogumYili = 1999;
+  double KrediOrani = 0.90;
+  bool izinli = true;
+
+  print(mesaj);    //Dart dilinde de ; gerekli
+  print(DogumYili);
+  print(KrediOrani);
+  print("Doğum Yılı: "+DogumYili.toString());
+  print("Oran: "+KrediOrani.toString());
+  print("Hello World");
 }
